b8e277bd05eb90ccbe91d45e57e544810173eba0
NJQ-PC <1853276674@qq.com>
2021-09-27 21:54:41 +0800

    失物招领V2.1.9

---
+++ app/src/main/java/com/example/mytestdemo/dialog/UserListView.java
@@ -0,0 +1,62 @@
+package com.example.mytestdemo.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.mytestdemo.R;
+import com.example.mytestdemo.User;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+
+public class UserListView extends Dialog {
+    List<User> usersList;
+    private ListView mUserList;
+
+    public UserListView(@NonNull @NotNull Context context) {
+        super(context);
+        setContentView(R.layout.userlist);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("用户列表");
+        initView();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                BmobQuery<User> bmobQuery = new BmobQuery<User>();
+                bmobQuery.order("-createdAt");
+                bmobQuery.findObjects(new FindListener<User>() {
+                    @Override
+                    public void done(List<User> object, BmobException e) {
+                        if (e == null) {
+                            usersList = object;
+                            Toast.makeText(getContext(), "" + object.size(), Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Log.i("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                        }
+                    }
+                });
+            }
+        }).start();
+        super.onCreate(savedInstanceState);
+    }
+
+    public void initView() {
+        mUserList = findViewById(R.id.user_list);
+    }
+}
--- app/src/main/java/com/example/mytestdemo/fragment/AdminUserList.java
+++ app/src/main/java/com/example/mytestdemo/fragment/AdminUserList.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -11,10 +12,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.mytestdemo.R;
+import com.example.mytestdemo.dialog.UserListView;
 
 
 public class AdminUserList extends Fragment {
     View view;
+    private Button mAddAdministrator;
+    private ListView mUserListview;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -22,11 +26,22 @@ public class AdminUserList extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable  Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.admin_user,container,false);
         ListView user_list=view.findViewById(R.id.user_listview);
-
-
+        initView();
+        mAddAdministrator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserListView list=new UserListView(requireActivity());
+                list.setCancelable(true);
+                list.show();
+            }
+        });
 
 
         return view;
     }
 
+    private void initView() {
+        mAddAdministrator = view.findViewById(R.id.add_administrator);
+        mUserListview = view.findViewById(R.id.user_listview);
+    }
 }
