77cc8acf64c1b86fc0f850b0599565301addc712
NanJiaQi315 <40727045+lmy8848@users.noreply.github.com>
2021-12-02 22:21:17 +0800

    改包名
    改

---
+++ app/src/main/java/com/qj315/QiJiu315/ui/activity/Qj315Page.java
@@ -0,0 +1,25 @@
+package com.qj315.QiJiu315.ui.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.mytestdemo.R;
+
+public class Qj315Page extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qj315_page);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Qj315Page.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
---
+++ app/src/main/java/com/qj315/QiJiu315/ui/adapter/ViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.qj315.QiJiu315.ui.adapter;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.qj315.QiJiu315.bean.FragmentInfo;
+
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<FragmentInfo> mFragments;
+
+    public ViewPagerAdapter(FragmentManager fm, List<FragmentInfo> fragments) {
+        super(fm);
+
+        mFragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+
+        try {
+            return (Fragment) mFragments.get(position).getFragment().newInstance();
+
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return  null;
+
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragments.get(position).getTitle();
+    }
+}
